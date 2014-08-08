b6a46ec5fa4bb245a51a542cbfd4897fa1a5432a
Michel Cote <tsfpuckeye@gmail.com>
2014-08-07 21:07:43 -0400

    Update #3

---
+++ FIDOM/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,18 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var Connexion = {};	// @login
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	Connexion.login = function Connexion_login (event)// @startlock
+	{// @endlock
+		// Add your code here
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("Connexion", "login", Connexion.login, "WAF");
+// @endregion
+};// @endlock
